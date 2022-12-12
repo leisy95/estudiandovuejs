3d77f8b28674b2800b6b7d84d46fd2188e453f5e
leisy95 <leisy95.ladino@gmail.com>
2022-12-12 13:49:46 -0500

    Ensayo capturas

---
+++ instruciones.js
@@ -0,0 +1,8 @@
+function captura(){
+    var nombrecont= document.getElementById("nombre").value
+    var emailcont= document.getElementById("email").value
+    var asuntocont= document.getElementById("asunto").value
+    var mensajecont= document.getElementById("mensaje").value
+
+    console.log(nombrecont, emailcont, asuntocont, mensajecont)
+}
